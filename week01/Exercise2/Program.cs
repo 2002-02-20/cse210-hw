e1d308ca0aa2e27ffd4bce14903294c576388afa
Derek <adhoc607@gmail.com>
2025-07-05 15:40:00 -0500

    Exercise 2 Completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade porcentage? ");
+        int note = int.Parse(Console.ReadLine());
+        string letter = " ";
+
+        if (note >= 90)
+        {
+            letter = "A";
+        }
+        else if (note >= 80)
+        {
+            letter = "B";
+        }
+        else if (note >= 70)
+        {
+            letter = "C";
+        }
+        else if (note >= 60)
+        {
+            letter = "D";
+        }
+        else if (note < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your note is: {letter}");
+
+        if (note >= 70)
+        {
+            Console.WriteLine("You passed. Congratulation!");
+            
+        }
+        else
+        {
+            Console.WriteLine("You don't passed");
+            Console.WriteLine("You need repeat this course");
+        }
+
     }
 }
