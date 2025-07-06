87bf2c9213d192e2bbacb695eb0e97f26e5369a6
Derek <adhoc607@gmail.com>
2025-07-05 19:44:10 -0500

    Exercise4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.ComponentModel.Design.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int writeNum;
+        int white = 0;
+        int a = 0;
+        int average = 0;
+        int largest = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            writeNum = int.Parse(Console.ReadLine());
+            if (writeNum != 0)
+            {
+                numbers.Add(writeNum);
+            }
+
+        } while (writeNum != 0);
+
+        foreach (int oneNum in numbers)
+        {
+            
+            white += oneNum;
+            if (largest < oneNum)
+            {
+                largest = oneNum;
+            }
+
+        }
+        a = numbers.Count;
+        if (a > 0)
+        {
+            average = white / a;
+        }
+
+        
+
+        Console.WriteLine($"The sum is: {white}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
