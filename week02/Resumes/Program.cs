5d8caa9247a787d7b6bbb123e6fb692c412457b2
Derek <adhoc607@gmail.com>
2025-07-08 22:15:31 -0500

    Abstraction-class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "NTT DATA";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2040;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2027;
+        job2._endYear = 2050;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Derek Moscui";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+        
     }
 }
