d6005ba012c58a9192389a7830b6c563ccb6f392
Derek <adhoc607@gmail.com>
2025-07-30 23:46:45 -0500

    HomeworkCompleted

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assimnet1 = new Assignment("Derek moscui", "Matte");
+        Console.WriteLine(assimnet1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Samuel Ruiz", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Sussan Storm","European History","The Causes of World War II");
+
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
